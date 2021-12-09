f35879a1948d95a6db219f49a85363395419b7e7
jensysantana <jensysantana@gmail.com>
2021-12-09 17:39:44 -0500

    test csrf token 1

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as csurf from 'csurf';
+import * as cookieParser from 'cookie-parser';
+import { VersioningType } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+  app.setGlobalPrefix('api/v1');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    // prefix: '/api/v1'
+  });
+  app.use(csurf({
+    cookie: {
+      key: 'XSRF-TOKEN',// can set a custome cookie name.
+      path: "/",
+      httpOnly: true,
+      secure: false,
+      // maxAge: -20000, // 14 days,
+      maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days,
+      // maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days,
+      sameSite: 'lax',
+      ignoreRoutes: ['admin', 'api'],
+
+      // signed: true,
+    },
+  }));
+  await app.listen(3590);
+}
+bootstrap();
